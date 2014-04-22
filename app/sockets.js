6ad2a3f046fb94a72135cd6b7e01649562425944
iahs <iahs@users.noreply.github.com>
2014-04-21 21:29:48 -0400

    Chat functionality

--- app/sockets.js
+++ app/sockets.js
@@ -2,9 +2,12 @@
  * Socket.io configuration
  */
 module.exports = function (io) {
+    var rooms = {main: []};
     io.sockets.on('connection', function(socket) {
         // Connect to the default room. By default, sockets join the room "" on connection
-        var currentRoom = {name: ""};
+        var currentRoom = {name: "main"};
+        socket.join(currentRoom.name);
+        socket.emit('server:roomStatus',rooms[currentRoom.name]);
 
         socket.on('join_room', function (room) {
             // TODO: Verify that user is authenticated and has access to room
@@ -16,7 +19,6 @@ module.exports = function (io) {
             // Join the new room
             socket.join(room.name);
             currentRoom = room;
-            socket.rooms
 
             socket.emit('server:message', {title: "You have joined " + room.name, sender: "RoomManager"});
         });
@@ -24,10 +26,21 @@ module.exports = function (io) {
         socket.on('chatclient:message', function(data) {
             console.log('message received ' + data['title']);
             socket.broadcast.to(currentRoom.name).emit('server:message', data);
+            rooms[currentRoom.name][data.topic].messages.push(data);
+        });
+
+        socket.on('chatclient:topic', function(data) {
+            console.log('topic received ' + data['title']);
+            socket.broadcast.to(currentRoom.name).emit('server:topic', data);
+            rooms[currentRoom.name].push(data);
         });
 
         socket.on('chartclient:series', function(data) {
             io.sockets.to(currentRoom.name).emit('chart:series', data);
+        });
+
+        socket.on('debug', function(data) {
+            console.log('debug: ' + data);
         });
     });
 };
--- public/js/directives.js
+++ public/js/directives.js
@@ -8,29 +8,69 @@ angular.module('nodePuzzles').directive('chatroom', function () {
         restrict: 'E',
         templateUrl: 'templates/chatroom.html',
         scope: {},
-        controller: function ($scope, $element, $attrs, socket) {
-            $scope.messages = [];
+        controller: function ($scope, $element, $attrs, socket, $location, $anchorScroll, $timeout) {
             $scope.newMessage = {sender: "Anonymous"};
+            $scope.newTopic = {sender: "Anonymous", title:""};
 
             // Send a new message
             $scope.sendMessage = function() {
                 if ($scope.newMessage.title != "") {
+                    $scope.newMessage.topic = $scope.activeTopic;
                     socket.emit('chatclient:message', $scope.newMessage);
                     var newMsg = angular.copy($scope.newMessage);
                     newMsg.class = "list-group-item-success";
-                    $scope.messages.push(newMsg);
+                    $scope.topics[$scope.activeTopic].messages.push(newMsg);
                     $scope.newMessage.title = "";
                 }
             };
 
+
+            // Create a new topic
+            $scope.createTopic = function() {
+                if ($scope.newTopic.title != "") {
+                    $scope.newTopic.index = $scope.nTopics++;
+                    $scope.newTopic.messages = [];
+                    socket.emit('chatclient:topic', $scope.newTopic);
+                    var newTopic = angular.copy($scope.newTopic);
+                    newTopic.class = "list-group-item-success";
+                    console.log($scope.topics)
+                    $scope.topics[newTopic.index]=newTopic;
+                    $scope.chooseTopic(newTopic.index);
+                    // Scroll to new topic
+                    $location.hash('topic-' + newTopic.index);
+                    $anchorScroll();
+                    $scope.newTopic.title = "";
+                }
+            };
+
+            // Set active topic
+            $scope.chooseTopic = function(index) {
+                $scope.activeTopic = index;
+            }
+
             // Receive a message
             socket.on('server:message', function(message) {
-                $scope.messages.push(message);
+                console.log('received: ' + message)
+                // Save scroll position to avoid moving off active topic
+                var scroll = $('#chat-container').scrollTop() - $('#topic-' + message.topic).height();
+                $scope.topics[message.topic].messages.push(message);
+                // Scroll to old position
+                if($scope.activeTopic !== undefined && message.topic < $scope.activeTopic)
+                    $timeout(function() {$('#chat-container').scrollTop(scroll + $('#topic-' + message.topic).height());});
             });
 
-            $scope.joinRoom = function() {
-                socket.emit('join_room', $scope.room );
-            };
+            // Receive a topic
+            socket.on('server:topic', function(topic) {
+                $scope.topics.push(topic);
+                $scope.nTopics++;
+            });
+
+            // Sync topic count
+            socket.on('server:roomStatus', function(data){
+                $scope.nTopics=data.length;
+                $scope.topics=data;
+                console.log(data)
+            });
 
         }
     }
